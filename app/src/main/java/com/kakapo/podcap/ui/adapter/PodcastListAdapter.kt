6117ed0bf8862cb728f8c4d992ec3281c1977d3a
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-05-01 10:11:18 +0700

    error cannot cast java.list to kotlin.sequence

--- app/src/main/java/com/kakapo/podcap/ui/adapter/PodcastListAdapter.kt
+++ app/src/main/java/com/kakapo/podcap/ui/adapter/PodcastListAdapter.kt
@@ -1,4 +1,62 @@
 package com.kakapo.podcap.ui.adapter
 
-class PodcastListAdapter {
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.kakapo.podcap.databinding.SearchItemBinding
+import com.kakapo.podcap.ui.viewmodel.SearchViewModel
+
+class PodcastListAdapter(
+    private var podcastSummaryViewList: List<SearchViewModel.PodcastSummaryViewData>?,
+    private val podcastListAdapterListener: PodcastListAdapterListener,
+    private val parentActivity: Activity
+) : RecyclerView.Adapter<PodcastListAdapter.ViewHolder>() {
+
+    interface PodcastListAdapterListener {
+        fun onShowDetails(podcastSummaryViewData: SearchViewModel.PodcastSummaryViewData)
+    }
+
+    inner class ViewHolder(view: SearchItemBinding) : RecyclerView.ViewHolder(view.root){
+        var podcastSummaryViewData: SearchViewModel.PodcastSummaryViewData? = null
+        val nameTextView = view.podcastNameTextView
+        val lastUpdateTextView = view.podcastLastUpdatedTextView
+        val podcastImageView = view.podcastImage
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding : SearchItemBinding = SearchItemBinding
+            .inflate(LayoutInflater.from(parentActivity), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val searchViewList = podcastSummaryViewList ?: return
+        val searchView = searchViewList[position]
+        holder.podcastSummaryViewData = searchView
+        holder.nameTextView.text = searchView.name
+        holder.lastUpdateTextView.text = searchView.lastUpdated
+        Glide.with(parentActivity)
+            .load(searchView.imageUrl)
+            .into(holder.podcastImageView)
+
+        holder.itemView.setOnClickListener {
+            holder.podcastSummaryViewData?.let {
+                podcastListAdapterListener.onShowDetails(it)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return podcastSummaryViewList?.size ?: 0
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun searchData(podcastSummaryViewData: List<SearchViewModel.PodcastSummaryViewData>){
+        podcastSummaryViewList = podcastSummaryViewData
+        this.notifyDataSetChanged()
+
+    }
 }
--- app/src/main/java/com/kakapo/podcap/ui/view/PodcastActivity.kt
+++ app/src/main/java/com/kakapo/podcap/ui/view/PodcastActivity.kt
@@ -1,13 +1,22 @@
 package com.kakapo.podcap.ui.view
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kakapo.podcap.R
 import com.kakapo.podcap.databinding.ActivityPodcastBinding
 import com.kakapo.podcap.ui.adapter.PodcastListAdapter
 import com.kakapo.podcap.ui.viewmodel.SearchViewModel
 
-class PodcastActivity : AppCompatActivity() {
+class PodcastActivity : AppCompatActivity(), PodcastListAdapter.PodcastListAdapterListener {
 
     private lateinit var mBinding: ActivityPodcastBinding
     private lateinit var mSearchViewModel: SearchViewModel
@@ -19,10 +28,39 @@ class PodcastActivity : AppCompatActivity() {
         setContentView(mBinding.root)
 
         mSearchViewModel = SearchViewModel()
-        podcastViewModelObserver()
-//        mPodcastViewModel.getPodcastFromAPi("Android developer")
-        mBinding.test.text = ("berhasil")
 
+        setupToolbar()
+        podcastViewModelObserver()
+        updateControls()
+        handleIntent(intent)
+//        mPodcastViewModel.getPodcastFromAPi("Android developer")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        val searchMenuItem = menu?.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onShowDetails(podcastSummaryViewData: SearchViewModel.PodcastSummaryViewData) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent!!)
+    }
+
+    private fun setupToolbar(){
+        setSupportActionBar(mBinding.toolbar)
     }
 
     private fun podcastViewModelObserver(){
@@ -44,5 +82,52 @@ class PodcastActivity : AppCompatActivity() {
             }
         })
     }
+
+    private fun performSearch(term: String){
+        showProgressBar()
+        mSearchViewModel.searchPodcastInAPI(term){result ->
+            hideProgressBar()
+            mBinding.toolbar.title = resources.getString(R.string.search_results)
+            podcastListAdapter.searchData(result)
+        }
+    }
+
+    private fun handleIntent(intent: Intent){
+        if(Intent.ACTION_SEARCH == intent.action){
+            val query = intent.getStringExtra(SearchManager.QUERY)
+            if (query != null){
+                performSearch(query)
+            }else{
+                Log.e("error search data", "cannot find any searching")
+            }
+        }
+    }
+
+    private fun updateControls(){
+        mBinding.podcastRecyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(this)
+        mBinding.podcastRecyclerView.layoutManager = layoutManager
+
+        val dividerItemDecoration = DividerItemDecoration(
+            mBinding.podcastRecyclerView.context, layoutManager.orientation
+        )
+        podcastListAdapter = PodcastListAdapter(
+            null,
+            this,
+            this
+        )
+
+        mBinding.podcastRecyclerView.addItemDecoration(dividerItemDecoration)
+    }
+
+    private fun showProgressBar(){
+        mBinding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar(){
+        mBinding.progressBar.visibility = View.GONE
+    }
+
 
 }
